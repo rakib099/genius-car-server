e27d25985d90dd98b70ae19bea84ea3014889aef
Rakib099 <rakibnabi97@gmail.com>
2022-12-16 23:05:47 +0600

    Deploy steps added

---
+++ _deploy-steps.js
@@ -0,0 +1,15 @@
+/*
+1. add vercel config (vercel.json file) to the root directory
+2. add: "build": "node index.js", in your package.json file
+3. vercel   (run this command)
+4. Questions
+    a) Set up and deploy? y
+    b) Which scope do you want to deploy to? Enter
+    c) Link to existing project? n
+    d) What’s your project’s name? Enter
+    e) In which directory is your code located? Enter
+    f) Want to modify these settings? n
+5) go to vercel => your project's settings => add environment variables
+6) deploy again using command: vercel
+7) update your localhost urls to vercel urls in your client-side project
+*/
